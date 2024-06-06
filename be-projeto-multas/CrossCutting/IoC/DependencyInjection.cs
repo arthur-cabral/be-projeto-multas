ba275bfb92852d1f1af38f43392b5f94bd59e2e5
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-06-06 02:30:43 -0300

    feature: Adding DependencyInjection manager class

---
+++ be-projeto-multas/CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Application.Services;
+using Application.Interfaces;
+using Application.Mapping;
+
+namespace CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var defaultConnection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    defaultConnection,
+                    b => b.MigrationsAssembly("API")
+                );
+            });
+
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IMultaService, MultaService>();
+
+            services.AddScoped<IMultaRepository, MultaRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
