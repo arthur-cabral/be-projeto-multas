c5de9c6ae28099d71e53e74951de5d2bd06414f4
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-06-06 02:29:38 -0300

    feature: Implementing Mapping of entity/DTO

---
+++ be-projeto-multas/Application/Mapping/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTO;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Multa, MultaDTO>().ReverseMap();
+        }
+    }
+}
