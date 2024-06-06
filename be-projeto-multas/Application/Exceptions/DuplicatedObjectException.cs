a574e031d77b0e839e4149c7ac41cf71645b93c1
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-06-06 02:28:35 -0300

    feature: Adding personalized exceptions

---
+++ be-projeto-multas/Application/Exceptions/DuplicatedObjectException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class DuplicatedObjectException : Exception
+    {
+        public DuplicatedObjectException() : base() { }
+
+        public DuplicatedObjectException(string message) : base(message) { }
+    }
+}
