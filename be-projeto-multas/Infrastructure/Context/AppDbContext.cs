52b423b394201d5b817fcaea54e71e4bcb064917
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-06-06 02:25:37 -0300

    feature: Adding AppDbContext to project

---
+++ be-projeto-multas/Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Application.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Multa> Multa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext)
+                .Assembly);
+        }
+    }
+}
