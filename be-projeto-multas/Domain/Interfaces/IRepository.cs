b3a3baf42a81d88d6e73a8871401a6821c084643
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-06-06 02:26:11 -0300

    feature: Creating generic class to repository

---
+++ be-projeto-multas/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Get();
+        Task<T> GetByProperty(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
